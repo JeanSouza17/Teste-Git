29d6c94ceaf23879a68e17b738cce1bd8f6674ef
Jean C Souza <jeancontrigiani@gmail.com>
2020-10-01 15:41:19 -0300

    Alterado nome da rota para teste7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/teste3', (req, res) => {
+app.get('/teste7', (req, res) => {
     return res.json({ hello: 'world 2'})
 });
 
